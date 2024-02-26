9f9cc52e2cf40da07089bb10e0a32fbd1b5dff85
antrinarich <antrinarich@gmail.com>
2024-02-26 10:16:01 +0100

    fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -18,3 +18,6 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+
+	return (0);
+}
